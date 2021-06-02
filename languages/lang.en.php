ff3e5ae49455ee620f9462a399b3ba4c3d0fb5b7
erhen3000 <erhen3000@gmx.com>
2021-06-02 15:32:12 +0200

    added language files

---
+++ languages/lang.en.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+$lang['lang'] = 'en';
+// H1
+$lang['h1'] = 'English title';
---
+++ languages/lang.nl.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+--------------------
+Language: Nederlands
+--------------------
+*/
+
+$lang = array();
+
+$lang['lang'] = 'nl';
+// H1
+$lang['h1'] = 'Nederlandse titel';
