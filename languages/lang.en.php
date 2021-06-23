58725598416ed34f225ff07e3b0b68f2b1caafce
erhen3000 <erhen3000@gmx.com>
2021-06-23 22:44:30 +0200

    added language tags and a counter

--- languages/lang.en.php
+++ languages/lang.en.php
@@ -7,6 +7,24 @@ Language: English
 
 $lang = array();
 
+// Language
 $lang['lang'] = 'en';
+
 // H1
 $lang['h1'] = 'English title';
+
+// Nav
+$lang['nav1'] = 'Students';
+$lang['nav2'] = 'Event Agenda';
+$lang['nav3'] = 'Course Scope';
+
+// Intro
+$lang['intro1'] = 'Back-end developers meet business partners';
+$lang['intro2'] = 'Register';
+
+// Counter
+$lang['counter1'] = 'Days';
+$lang['counter2'] = 'Hours';
+$lang['counter3'] = 'Minutes';
+$lang['counter4'] = 'Seconds';
+
