54d9a4a8f09aa870b9c9aa95e2bd9e6bce9d67c5
erhen3000 <erhen3000@gmx.com>
2021-06-23 19:28:43 +0200

    removed phpmailer

--- .con/creds.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "opleidingphpgenk@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "";
-?>
