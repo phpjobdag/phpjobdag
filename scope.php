853e2c52199945610b110720ba6cd7c9a360e24b
erhen3000 <erhen3000@gmx.com>
2021-06-26 17:28:53 +0200

    footer and scope

--- scope.php
+++ scope.php
@@ -7,47 +7,49 @@
 
         <h3 class="course__hard__title">Hard-Skills</h3>
 
-        <article class="course__hard__front">
-            <h6 class="course__hard__front__title">Front-end Development</h6>
-            <img class="course__hard__front__img" src="img/skills__html5.svg" alt="html5 logo">
-            <img class="course__hard__front__img" src="img/skills__css3.svg" alt="css3 logo">
-            <img class="course__hard__front__img" src="img/skills__js.svg" alt="js logo">
-            <img class="course__hard__front__img" src="img/skills__bootstrap.svg" alt="bootstrap logo">
-        </article>
+        <section class="course_hard_front_back">
+            <article class="course__hard__front">
+                <h6 class="course__hard__front__title">Front-end Development</h6>
+                <img class="course__hard__front__img" src="img/skills__html5.svg" alt="html5 logo">
+                <img class="course__hard__front__img" src="img/skills__css3.svg" alt="css3 logo">
+                <img class="course__hard__front__img" src="img/skills__js.svg" alt="js logo">
+                <img class="course__hard__front__img" src="img/skills__bootstrap.svg" alt="bootstrap logo">
+            </article>
 
-        <article class="course__hard__back">
-            <h6 class="course__hard__back__title">Back-end Development</h6>
-            <img class="course__hard__back__img" src="img/skills__php.svg" alt="">
-            <img class="course__hard__back__img" src="img/skills__mysql.svg" alt="">
-            <img class="course__hard__back__img" src="img/skills__laravel.svg" alt="">
-        </article>
+            <article class="course__hard__back">
+                <h6 class="course__hard__back__title">Back-end Development</h6>
+                <img class="course__hard__back__img" src="img/skills__php.svg" alt="">
+                <img class="course__hard__back__img" src="img/skills__mysql.svg" alt="">
+                <img class="course__hard__back__img" src="img/skills__laravel.svg" alt="">
+            </article>
+        </section>
+        <section class="course__hard__edit__control">
+            <article class="course__hard__edit">
+                <h6 class="course__hard__edit__title">Code editors</h6>
+                <img class="course__hard__edit__img" src="img/skills__vsc.svg" alt="vsc logo">
+                <img class="course__hard__edit__img" src="img/skills__phpstorm.svg" alt="php storm logo">
+            </article>
 
-        <article class="course__hard__edit">
-            <h6 class="course__hard__edit__title">Code editors</h6>
-            <img class="course__hard__edit__img" src="img/skills__vsc.svg" alt="vsc logo">
-            <img class="course__hard__edit__img" src="img/skills__phpstorm.svg" alt="php storm logo">
-        </article>
-
-        <article class="course__hard__control">
-            <h6 class="course__hard__control__title">Version Control</h6>
-            <img class="course__hard__control__img" src="img/skills__github.svg" alt="github logo">
-        </article>
-
+            <article class="course__hard__control">
+                <h6 class="course__hard__control__title">Version Control</h6>
+                <img class="course__hard__control__img" src="img/skills__github.svg" alt="github logo">
+            </article>
+        </section>
     </section>
 
-    <section class="course__soft">
-
+    <section class="course__soft margBot0">
         <h3 class="course__soft__title">Soft-Skills</h3>
-        <article class="agile-method">
-            <h6 class="course__soft__agile__title">Agile Tools</h6>
-            <img class="course__soft__agile__img" src="img/skills__trello.svg" alt="trello logo">
-        </article>
-        <article class="remote-working">
-            <h6 class="course__soft__remote__title">Remote Working</h6>
-            <img class="course__soft__remote__img" src="img/skills__zoom.svg" alt="zoom logo">
-            <img class="course__soft__remote__img" src="img/skills__slack.svg" alt="slack logo">
-            <img class="course__soft__remote__img" src="img/skills__discord.svg" alt="discord logo">
-        </article>
-        
+        <section class="agile__remote">
+            <article class="agile-method">
+                <h6 class="course__soft__agile__title">Agile Tools</h6>
+                <img class="course__soft__agile__img" src="img/skills__trello.svg" alt="trello logo">
+            </article>
+            <article class="remote-working">
+                <h6 class="course__soft__remote__title">Remote Working</h6>
+                <img class="course__soft__remote__img" src="img/skills__zoom.svg" alt="zoom logo">
+                <img class="course__soft__remote__img" src="img/skills__slack.svg" alt="slack logo">
+                <img class="course__soft__remote__img" src="img/skills__discord.svg" alt="discord logo">
+            </article>
+        </section>
     </section>
 </section>
