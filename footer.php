3faec18ba411c3c5d832536e3875041e721dca9f
Daniel Paz <paz_daniele@hotmail.com>
2021-07-09 09:04:07 +0200

    - Footer herwerkt grid -> flexbox (dubbele divs verwijderd) - Tussentitels responsive gemaakt en marges fluid gezet - Zwarte blob uitgeschakeld (vergt andere aanpak) - Marges toegevoegd aan footer sectie

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <h2 class="footer__title"><?php echo $lang['footerTitle']; ?></h2>
 
     <div id="footer__wrapper">
-    <section id="signup" class="footer__register"> 
+    <section id="signup" class="footer__register">
         <article class="footer__register__info">
             <h3 class="footer__register__info__title"><?php echo $lang['footerSubtitle']; ?></h3>
             <p class="footer__register__info__location"><?php echo $lang['footerBody1']; ?></h3>
@@ -69,18 +69,14 @@
              <input class="footer__register__form__submit form__submit"
                     type="submit" value="<?php echo $lang['formSubmit']; ?>">
         </form>
-       
+
     </section>
 
     <section class="footer__info">
-        <article class="footer__info__thx">
-            <h6 class="footer__info__thx__title"><?php echo $lang['footerThank1']; ?></h6>
-            <p class="footer__info__thx__body"><?php echo $lang['footerThank2']; ?></p>
-        </article>
+<!--        <section class="footer__info__team__partners__container">-->
 
-        <section class="footer__info__team__partnerts__container">
             <article class="footer__info__team">
-                <h6 class="footer__info__team__title">Project Team</h6>
+                <h6 class="footer__subtitle">Project Team</h6>
                 <ul class="footer__info__team__list">
                     <li class="footer__info__team__list__name">Alexander Decoz</li>
                     <li class="footer__info__team__list__name">Anastasya Ovchar</li>
@@ -97,82 +93,124 @@
             </article>
 
 
-            <article class="footer__info__partners">
-                <h6 class="footer__info__partners__title">Partners</h6>
+            <div class="footer__info__second__column__flex">
+                <article class="footer__info__thx">
+                    <h6 class="footer__subtitle"><?php echo $lang['footerThank1']; ?></h6>
+                    <p class="footer__info__thx__body"><?php echo $lang['footerThank2']; ?></p>
+                </article>
 
-                <a href="<?php echo $lang['FootPartnerMULTIMEDI']; ?>"><img class="footer__info__partners__image" src="img/partners__multimedi.svg" alt="logo multimedi"></a>
-                <a href="<?php echo $lang['FootPartnerCEVORA']; ?>"><img  class="footer__info__partners__image" src="img/partners__cevora.svg" alt="logo cevora"></a>
 
-            </article>
-        </section>
+                <div class="footer__info__partners__tools__flex">
+                    <article class="footer__info__partners">
+                        <h6 class="footer__subtitle">Partners</h6>
 
-        <!-- desktopsite geen container -->
-        <article class="footer__info__team no__display__mobile">
-                <h6 class="footer__info__team__title">Project Team</h6>
-                <ul class="footer__info__team__list">
-                    <li class="footer__info__team__list__name">Alexander Decoz</li>
-                    <li class="footer__info__team__list__name">Anastasya Ovchar</li>
-                    <li class="footer__info__team__list__name">Arne Sneyers</li>
-                    <li class="footer__info__team__list__name">Burcu Avci</li>
-                    <li class="footer__info__team__list__name">Daniel Paz</li>
-                    <li class="footer__info__team__list__name">Erik Hendriks</li>
-                    <li class="footer__info__team__list__name">Geert Rumen</li>
-                    <li class="footer__info__team__list__name">Mathijs Reykers</li>
-                    <li class="footer__info__team__list__name">Miguel Duarte</li>
-                    <li class="footer__info__team__list__name">Wouter Swerts</li>
-                    <li class="footer__info__team__list__name">Yves Kerkhofs</li>
-                </ul>
-            </article>
 
-            <article class="footer__info__partners no__display__mobile">
-                <h6 class="footer__info__partners__title">Partners</h6>
-                <img class="footer__info__partners__image" src="img/partners__multimedi.svg" alt="logo multimedi">
-                <img  class="footer__info__partners__image" src="img/partners__cevora.svg" alt="logo cevora">
-            </article>
+                        <div class="footer__info__partners__logos__flex">
+                            <img class="footer__info__partners__image"
+                                 src="img/partners__multimedi.svg"
+                                 alt="logo multimedi">
 
-        <article class="footer__info__tools">
-            <h6 class="footer__info__tools__title"><?php echo $lang['footerMade']; ?></h6>
-            <div class="footer__info__tools__static">
-                <div class="footer__info__tools__moving">
-                    <img class="footer__info__tools__img" src="img/skills__css3.svg" alt="css3 logo">
-                    <img class="footer__info__tools__img" src="img/skills__html5.svg" alt="html5 logo">
-                    <img class="footer__info__tools__img" src="img/skills__js.svg" alt="js logo">
-                    <img class="footer__info__tools__img" src="img/skills__vsc.svg" alt="vsc logo">
-                    <img class="footer__info__tools__img" src="img/skills__phpstorm.svg" alt="PHP storm logo">
-                    <img class="footer__info__tools__img" src="img/skills__angular.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__bootstrap.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__discord.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__github.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__laravel.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__mysql.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__php.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__slack.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__trello.svg" alt="">
-                    <img class="footer__info__tools__img" src="img/skills__zoom.svg" alt="">
-                    <!--extra logo's zijn nodig om de ononderbroken beweging te garanderen-->
-                    <img class="footer__info__tools__img" src="img/skills__css3.svg" alt="css3 logo">
-                    <img class="footer__info__tools__img" src="img/skills__html5.svg" alt="html5 logo">
-                    <img class="footer__info__tools__img" src="img/skills__js.svg" alt="js logo">
-                    <img class="footer__info__tools__img" src="img/skills__vsc.svg" alt="vsc logo">
-                    <img class="footer__info__tools__img" src="img/skills__phpstorm.svg" alt="PHP storm logo">
+                            <img  class="footer__info__partners__image"
+                                  src="img/partners__cevora.svg"
+                                  alt="logo cevora">
+                        </div>
+                    </article>
+
+                    <article class="footer__info__tools">
+                        <h6 class="footer__subtitle"><?php echo $lang['footerMade']; ?></h6>
+                        <div class="footer__info__tools__static">
+                            <div class="footer__info__tools__moving">
+                                <img class="footer__info__tools__img" src="img/skills__css3.svg" alt="css3 logo">
+                                <img class="footer__info__tools__img" src="img/skills__html5.svg" alt="html5 logo">
+                                <img class="footer__info__tools__img" src="img/skills__js.svg" alt="js logo">
+                                <img class="footer__info__tools__img" src="img/skills__vsc.svg" alt="vsc logo">
+                                <img class="footer__info__tools__img" src="img/skills__phpstorm.svg" alt="PHP storm logo">
+                                <img class="footer__info__tools__img" src="img/skills__angular.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__bootstrap.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__discord.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__github.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__laravel.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__mysql.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__php.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__slack.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__trello.svg" alt="">
+                                <img class="footer__info__tools__img" src="img/skills__zoom.svg" alt="">
+                                <!--extra logo's zijn nodig om de ononderbroken beweging te garanderen-->
+                                <img class="footer__info__tools__img" src="img/skills__css3.svg" alt="css3 logo">
+                                <img class="footer__info__tools__img" src="img/skills__html5.svg" alt="html5 logo">
+                                <img class="footer__info__tools__img" src="img/skills__js.svg" alt="js logo">
+                                <img class="footer__info__tools__img" src="img/skills__vsc.svg" alt="vsc logo">
+                                <img class="footer__info__tools__img" src="img/skills__phpstorm.svg" alt="PHP storm logo">
+                            </div>
+                        </div>
+                    </article>
                 </div>
             </div>
-        </article>
+
+
+            <section class="footer__info__third__column__flex  footer__lang">
+                <ul class="footer__lang__lang">
+                    <li class="footer__lang__lang__switch"><a id="footer__en" href="?lang=en">EN</a></li>
+                    <li class="footer__lang__lang__switch bg__white"><a id="footer__nl"href="?lang=nl">NL</a></li>
+                </ul>
+            </section>
+
+<!--        </section>-->
+
+
+
+        <!--            <article class="footer__info__partners">-->
+        <!--                <h6 class="footer__subtitle">Partners</h6>-->
+        <!---->
+        <!--                <a href="--><?php //echo $lang['FootPartnerMULTIMEDI']; ?><!--"><img class="footer__info__partners__image" src="img/partners__multimedi.svg" alt="logo multimedi"></a>-->
+        <!--                <a href="--><?php //echo $lang['FootPartnerCEVORA']; ?><!--"><img  class="footer__info__partners__image" src="img/partners__cevora.svg" alt="logo cevora"></a>-->
+        <!---->
+        <!--            </article>-->
+
+        <!-- desktopsite geen container -->
+<!--        <article class="footer__info__team no__display__mobile">-->
+<!--                <h6 class="footer__subtitle">Project Team</h6>-->
+<!--                <ul class="footer__info__team__list">-->
+<!--                    <li class="footer__info__team__list__name">Alexander Decoz</li>-->
+<!--                    <li class="footer__info__team__list__name">Anastasya Ovchar</li>-->
+<!--                    <li class="footer__info__team__list__name">Arne Sneyers</li>-->
+<!--                    <li class="footer__info__team__list__name">Burcu Avci</li>-->
+<!--                    <li class="footer__info__team__list__name">Daniel Paz</li>-->
+<!--                    <li class="footer__info__team__list__name">Erik Hendriks</li>-->
+<!--                    <li class="footer__info__team__list__name">Geert Rumen</li>-->
+<!--                    <li class="footer__info__team__list__name">Mathijs Reykers</li>-->
+<!--                    <li class="footer__info__team__list__name">Miguel Duarte</li>-->
+<!--                    <li class="footer__info__team__list__name">Wouter Swerts</li>-->
+<!--                    <li class="footer__info__team__list__name">Yves Kerkhofs</li>-->
+<!--                </ul>-->
+<!--            </article>-->
+
+
+
+
+        <!--  SKILLS SLIDER   -->
+
     </section>
 
-    <section class="footer__lang">
-        <ul class="footer__lang__lang">
-            <li class="footer__lang__lang__switch"><a id="footer__en" href="?lang=en">EN</a></li>
-            <li class="footer__lang__lang__switch bg__white"><a id="footer__nl"href="?lang=nl">NL</a></li>
-        </ul>
-    </section>
+        <!--  LANGUAGE BUTTONS  -->
+
+
+
+
+
 
     <section class="footer__copyright">
-        <p class="footer__copyright__text">&copy; <?php echo date("Y");?> PHP</p>
-        <p class="footer__copyright__corporation"></p>
+        <div class="footer__copyright__wrapper">
+            <p class="footer__copyright__text">&copy; <?php echo date("Y");?> PHP</p>
+            <p class="footer__copyright__corporation"></p>
+        </div>
         <img class="footer__copyright__vdablogo" src="img/partners__vdab.svg" alt="">
     </section>
-    
+
+
+
+        <!--  BLOBS   -->
+
     <div class="fluid2">
         <svg id="footer__blob__black" viewBox="0 0 1440 687">
             <path d="M-16 0C136.549 61.7737 289.408 123.547 474.138 150.393C659.178 177.239 876.089 169.012 1038.54 210.579C1201.3 252.002 1309.29 343.364 1375.51 433.57C1441.73 523.922 1465.86 613.118 1490 702.459H-16V0Z" fill="#001220"/>
