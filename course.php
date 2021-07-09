ca60748cddf336ab08b976cf9dc0512d9ebe784e
Daniel Paz <paz_daniele@hotmail.com>
2021-07-09 15:38:53 +0200

    - Course section breaks in skill__titles - Made type responsive

--- course.php
+++ course.php
@@ -17,7 +17,7 @@
         <section class="course_hard_front_back">
             <article class="course__hard__front">
 
-                <h6 class="course__hard__front__title">Front-end Development</h6>
+                <h6 class="course__hard__front__title skill__title">Front-end<br> Development</h6>
                 <a href="<?php echo $lang['HardSkillHTML']; ?>" target="_blank"><img class="course__hard__front__img" src="img/skills__html5.svg" alt="html5 logo"></a>
                 <a href="<?php echo $lang['HardSkillCSS']; ?>" target="_blank"><img class="course__hard__front__img" src="img/skills__css3.svg" alt="css3 logo"></a>
                 <a href="<?php echo $lang['HardSkillJS']; ?>" target="_blank"><img class="course__hard__front__img" src="img/skills__js.svg" alt="js logo"></a>
@@ -28,7 +28,7 @@
             </article>
 
             <article class="course__hard__back">
-                <h6 class="course__hard__back__title">Back-end Development</h6>
+                <h6 class="course__hard__back__title skill__title">Back-end<br> Development</h6>
                 <a href="<?php echo $lang['HardSkillPHP']; ?>" target="_blank"><img class="course__hard__back__img" src="img/skills__php.svg" alt=""></a>
                 <a href="<?php echo $lang['HardSkillPHPUNIT']; ?>" target="_blank"><img class="course__hard__back__img" src="img/skills__phpunit.svg" alt=""></a>
                 <a href="<?php echo $lang['HardSkillMYSQL']; ?>" target="_blank"><img class="course__hard__back__img" src="img/skills__mysql.svg" alt=""></a>
@@ -44,14 +44,14 @@
         <!--  FRONT AND BACK END SECTION  -->
         <section class="course__hard__edit__control">
             <article class="course__hard__edit">
-                <h6 class="course__hard__edit__title">Code editors</h6>
+                <h6 class="course__hard__edit__title skill__title">Code<br> editors</h6>
                 <a href="<?php echo $lang['HardSkillVSC']; ?>" target="_blank"><img class="course__hard__edit__img" src="img/skills__vsc.svg" alt="vsc logo"></a>
                 <a href="<?php echo $lang['HardSkillPHPS']; ?>" target="_blank"><img class="course__hard__edit__img" src="img/skills__phpstorm.svg" alt="php storm logo"></a>
 
             </article>
 
             <article class="course__hard__control">
-                <h6 class="course__hard__control__title skill__title">Version Control</h6>
+                <h6 class="course__hard__control__title skill__title">Version<br> Control</h6>
 
                 <a href="<?php echo $lang['HardSkillGITHUB']; ?>" target="_blank"><img class="course__hard__control__img" src="img/skills__github.svg" alt="github logo"></a>
                 <a href="<?php echo $lang['HardSkillGITLAB']; ?>" target="_blank"><img class="course__hard__control__img" src="img/skills__gitlab.svg" alt="gitlab logo"></a>
@@ -71,12 +71,12 @@
         <section class="agile__remote">
             <article class="agile-method">
 
-                <h6 class="course__soft__agile__title skill__title">Agile Tools</h6>
+                <h6 class="course__soft__agile__title skill__title">Agile<br> Tools</h6>
                 <a href="<?php echo $lang['HardSkillTRELLO']; ?>" target="_blank"><img class="course__soft__agile__img" src="img/skills__trello.svg" alt="trello logo"></a>
             </article>
 
             <article class="remote-working">
-                <h6 class="course__soft__remote__title skill__title">Remote Working</h6>
+                <h6 class="course__soft__remote__title skill__title">Remote<br> Working</h6>
                 <a href="<?php echo $lang['HardSkillZOOM']; ?>" target="_blank"><img class="course__soft__remote__img" src="img/skills__zoom.svg" alt="zoom logo"></a>
                 <a href="<?php echo $lang['HardSkillSLACK']; ?>" target="_blank"><img class="course__soft__remote__img slack" src="img/skills__slack.svg" alt="slack logo"></a>
                 <a href="<?php echo $lang['HardSkillDISCORD']; ?>" target="_blank"><img class="course__soft__remote__img" src="img/skills__discord.svg" alt="discord logo"></a>
