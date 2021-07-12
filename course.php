0cdd593153beaa1a287abcba7ea95171afcbb7a0
erhen3000 <erhen3000@gmx.com>
2021-07-12 20:54:11 +0200

    cleanup course

--- course.php
+++ course.php
@@ -1,7 +1,6 @@
 <section id="course" class="course">
 
-    <h2 class="course__title"><?php echo $lang['courseTitle']; ?></h2>
-
+<h2 class="course__title"><?php echo $lang['courseTitle']; ?></h2>
 
  <div class="margin__wrapper">
 
@@ -12,9 +11,7 @@
 
          <div class="skills__wrapper__flex">
              <!--  HARD SKILLS SECTION   -->
-<!--             <h3 class="course__soft__title">Skills</h3>-->
              <section class="course__hard">
-<!--                 <h3 class="course__hard__title">Hard<br>Skills</h3>-->
 
                  <!--  FRONT AND BACK END DEVELOPMENT SECTION  -->
                  <section class="course_hard_front_back">
@@ -27,7 +24,6 @@
                          <a href="<?php echo $lang['HardSkillBS']; ?>" target="_blank"><img class="course__hard__front__img" src="img/skills__bootstrap.svg" alt="bootstrap logo"></a>
                          <a href="<?php echo $lang['HardSkillANGULAR'];  ?>" target="_blank"><img class="course__hard__front__img" src="img/skills__angular.svg" alt="angular logo"></a>
 
-
                      </article>
 
                      <article class="course__hard__back">
@@ -38,39 +34,14 @@
                          <a href="<?php echo $lang['HardSkillWAMP']; ?>" target="_blank"><img class="course__hard__back__img" src="img/skills__wamp.svg" alt="WAMP logo"></a>
                          <a href="<?php echo $lang['HardSkillLARAVEL']; ?>" target="_blank"><img class="course__hard__back__img" src="img/skills__laravel.svg" alt="Laravel logo"></a>
 
-
-
-
                      </article>
                  </section>
                  <!--  END OF FRONT AND BACK END SECTION  -->
-
-                 <!--  FRONT AND BACK END SECTION  -->
-<!--                 <section class="course__hard__edit__control">-->
-<!--                     <article class="course__hard__edit">-->
-<!--                         <h6 class="course__hard__edit__title skill__title">Code<br> editors</h6>-->
-<!--                         <a href="--><?php //echo $lang['HardSkillVSC']; ?><!--" target="_blank"><img class="course__hard__edit__img" src="img/skills__vsc.svg" alt="vsc logo"></a>-->
-<!--                         <a href="--><?php //echo $lang['HardSkillPHPS']; ?><!--" target="_blank"><img class="course__hard__edit__img" src="img/skills__phpstorm.svg" alt="php storm logo"></a>-->
-<!---->
-<!--                     </article>-->
-<!---->
-<!--                     <article class="course__hard__control">-->
-<!--                         <h6 class="course__hard__control__title skill__title">Version<br> Control</h6>-->
-<!---->
-<!--                         <a href="--><?php //echo $lang['HardSkillGITHUB']; ?><!--" target="_blank"><img class="course__hard__control__img" src="img/skills__github.svg" alt="github logo"></a>-->
-<!--                         <a href="--><?php //echo $lang['HardSkillGITLAB']; ?><!--" target="_blank"><img class="course__hard__control__img" src="img/skills__gitlab.svg" alt="gitlab logo"></a>-->
-<!---->
-<!--                     </article>-->
-<!--                 </section>-->
-
              </section>
              <!--    END OF HARD SKILLS SECTION   -->
 
-
              <!--    SOFT SKILLS SECTION     -->
              <section class="course__soft margBot0">
-<!--                 <h3 class="course__soft__title">Soft<br>Skills</h3>-->
-
                  <!--  FRONT AND BACK END SECTION  -->
                  <section class="course__hard__edit__control">
                      <article class="course__hard__edit">
@@ -88,7 +59,6 @@
 
                      </article>
                  </section>
-
 
                  <section class="agile__remote">
                      <article class="agile-method">
@@ -110,7 +80,6 @@
 
          </div>
  </div>
-
 
     <svg id="course__blob__yellow__bottom" viewBox="0 -7 1389 1310">
     <path class="yellow" d="M0 0C150.092 79.1279 300.183 158.525 484.801 205.894C669.705 253.263 889.135 268.604 982.158 383.528C1075.18 498.721 1042.08 713.497 1088.88 884.671C1135.39 1055.58 1262.08 1182.61 1388.77 1309.92H0V0Z" fill="#FBAE3C"/>
