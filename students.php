78ff4fa5e333bf88df576826358fca5b93e31197
burcuavci <71653593+burcuavci@users.noreply.github.com>
2021-07-13 16:11:05 +0200

    Update students.php
    Burcu > CV, personal website, Linkedin links added

--- students.php
+++ students.php
@@ -49,9 +49,9 @@
             <h5 class="student__card__name">Burcu<br>Avci</h5>
             <p class="student__card__pitch"><?php echo $lang['Burcu']; ?></p>
             <section class="students__card__social">
-                <a class="students__card__social__item" href=""><img class="students__card__social__item__img" src="img/card__cv.svg" alt="cv icon"></a>
-                <a class="students__card__social__item" href=""><img class="students__card__social__item__img" src="img/card__website.svg" alt="website icon"></a>
-                <a class="students__card__social__item" href=""><img class="students__card__social__item__img" src="img/card__linkedin.svg" alt="linkedin icon"></a>
+                <a class="students__card__social__item" href="https://burcuavci.com/resume"><img class="students__card__social__item__img" src="img/card__cv.svg" alt="cv icon"></a>
+                <a class="students__card__social__item" href="https://burcuavci.com/"><img class="students__card__social__item__img" src="img/card__website.svg" alt="website icon"></a>
+                <a class="students__card__social__item" href="https://www.linkedin.com/in/burcuavci/"><img class="students__card__social__item__img" src="img/card__linkedin.svg" alt="linkedin icon"></a>
             </section>
         </section>
     </article>
